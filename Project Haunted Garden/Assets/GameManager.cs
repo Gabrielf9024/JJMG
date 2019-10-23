1f3c7da67eac261fe149cbf1c24ec83866c01cce
Matthew Gonzales <matthekg@uci.edu>
2019-10-23 03:36:11 -0700

    Added small prototype qol
    Pause button and walls

---
+++ Project Haunted Garden/Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool paused;
+    public GameObject pauseMenu;
+
+
+    private void Awake()
+    {
+        pauseMenu = GameObject.Find("Quit");
+    }
+    private void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Escape)){
+            paused = !paused;
+        }
+
+        if( paused )
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void EndGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ClearScreen()
+    {
+        GameObject[] bullets;
+        bullets = GameObject.FindGameObjectsWithTag("Projectile");
+
+        foreach (GameObject bullet in bullets)
+        {
+            Destroy(bullet);
+        }
+    }
+
+}
