3c6b8cf27739f7c5841069935dd8db44fa0dd570
Gabriel Fuentes <gabrielf9024@gmail.com>
2019-10-29 23:26:33 -0700

    File for enemy

---
+++ Project Haunted Garden/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+	public GameObject EnemySprite;
+	public int Max;
+	public int count;
+	public bool horde;
+	private float mtime;
+    void Start(){
+    	Max = 30;
+    	count = 0;
+    	mtime = 2;
+    	horde = false;
+    }
+    void Update()
+    {
+    	mtime -= Time.deltaTime;
+    	float xAxis = Random.Range(-9.0f,9.0f);
+    	if (mtime <= 0f){
+    		EnemySpawner(xAxis);
+    		mtime = 2f;
+    	}
+    }
+
+    public void EnemySpawner(float Position_x){
+    	 if (count < Max){
+    	 	int HordeNumber = Random.Range(2,7);
+    	 	if (Random.Range(0f,1f) > 0.9 && horde){
+    	 		for (int num = 0; num < HordeNumber; num++){    	 			
+    	 			Vector3 NewSpritePosition = new Vector3(Position_x + Random.Range(-1f,1f),-4,0);
+	    			Instantiate(EnemySprite,NewSpritePosition,Quaternion.identity);
+	    			count++;
+    	 		}
+    	 	}
+    	 	else{ 
+		    	Vector3 NewSpritePosition = new Vector3(Position_x,-4,0);
+		    	Instantiate(EnemySprite,NewSpritePosition,Quaternion.identity);
+		    	count++;
+	    	}
+	    }
+    }
+}
