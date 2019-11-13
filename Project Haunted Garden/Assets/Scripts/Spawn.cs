1ac3ee73f6d0a9cffe5d833dfa882a2e15b7e299
Matthew Gonzales <matthekg@uci.edu>
2019-11-13 11:17:36 -0800

    Created a new Waypoint system
    Path enemies will follow the waypoints in order once spawned in. Waypoints can be moved around and enemies will follow. Created a sub enemy type that follows the path.

---
+++ Project Haunted Garden/Assets/Scripts/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public List<Dictionary<GameObject, int>> SpawnList;
+    public float delayInterval = 1.0f;
+    public int amountToSpawn = 1;
+
+    public GameObject pathEnemy;
+    public GameObject flyEnemy;
+
+    void OnEnable()
+    {
+        StartSpawning();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator StartSpawning()
+    {
+        Instantiate(pathEnemy);
+        yield return new WaitForSeconds(delayInterval);
+    }
+}
---
+++ Project Haunted Garden/Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    public List<GameObject> Waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Waypoints.Reverse();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
