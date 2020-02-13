d71f1893079153df77ea4666f331dd54d0635ab2
Gabriel Fuentes <gabrielf9024@gmail.com>
2020-02-12 20:12:24 -0800

    Dialogue Script
    Input dialogue and choose who is talking

--- Project Haunted Garden/Assets/Scripts/GameDialogueScripts/DialogueManager.cs
+++ Project Haunted Garden/Assets/Scripts/GameDialogueScripts/DialogueManager.cs
@@ -7,16 +7,20 @@ using TMPro;
 public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI textD;
-    //public string[] sentence;
-    private int index;
-    public float speed;
-    public int levelNumber;
-    public bool isPlayer;
-
     public GameObject continueButton;
     public GameObject TextBox;
+    public int index;
+    public int level;
+    public float Textspeed;
 
-    
+    [System.Serializable]
+    public class DialogueListPerLevel
+    {
+        public bool  isplayer;
+        public string[] dialogue;
+    }
+    public DialogueListPerLevel[] DialogueList;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,38 +29,43 @@ public class DialogueManager : MonoBehaviour
 
     IEnumerator Type()
     {
-        foreach(char letter in sentenceList()[index].ToCharArray())
+        if (index < DialogueList[level].dialogue.Length)
         {
-            textD.text += letter;
-            yield return new WaitForSeconds(speed);
+            foreach (char letter in DialogueList[level].dialogue[index].ToCharArray())
+            {
+                textD.text += letter;
+                yield return new WaitForSeconds(Textspeed);
+            }
         }
     }
 
-    // Update is called once per frame
      void Update()
     {
-        if (isPlayer)
+        if (index < DialogueList[level].dialogue.Length)
         {
-            TextBox.GetComponent<Image>().color = new Color32(255, 255, 225, 100);
-        }
-        else
-        {
-            TextBox.GetComponent<Image>().color = new Color32(214, 210, 191, 255);
-        }
-        if (textD.text == sentenceList()[index])
-        {
-            continueButton.SetActive(true);
+            if (DialogueList[level].isplayer)
+            {
+                TextBox.GetComponent<Image>().color = new Color32(214, 210, 191, 255);
+            }
+            else
+            {
+                TextBox.GetComponent<Image>().color = new Color32(200, 110, 141, 255);
+            }
+            if (textD.text == DialogueList[level].dialogue[index])
+            {
+                continueButton.SetActive(true);
+            }
         }
     }   
     
     public void nextLine()
     {
         continueButton.SetActive(false);
-        if (index < sentenceList().Length - 1)
+        if (index < DialogueList[level].dialogue[index].Length - 1)
         {
-            index++;
+           index++;
             textD.text = "";
-            isPlayer = !isPlayer;
+            DialogueList[level].isplayer = !DialogueList[level].isplayer;
             StartCoroutine(Type());
         }
         else
@@ -65,34 +74,4 @@ public class DialogueManager : MonoBehaviour
             continueButton.SetActive(false);
         }
     }
-    public string[] sentenceList()
-    {
-        if (levelNumber == 1)
-        {
-            string[] text = {"Hello world will you help me do this assigment", "else i eill comittadfjjsdlk kwejkahkjdfneidm "};
-            return text;
-        }
-        if (levelNumber == 2)
-        {
-            string[] text = { "ldskfalksdfkjhkajsdfkjakjshdfkjaksdhkfj", "adfasdfasdfasdfasdfasdfasd","dfasdfasdfasdfasdfasdfas" };
-            return text;
-        }
-        if (levelNumber == 3)
-        {
-            string[] text = { "ldskfalksdfkjhkajsdfkjakjshdfkjaksdhkfj", "adfasdfasdfasdfasdfasdfasd", "dfasdfasdfasdfasdfasdfas" };
-            return text;
-        }
-        if (levelNumber == 4)
-        {
-            string[] text = { "ldskfalksdfkjhkajsdfkjakjshdfkjaksdhkfj", "adfasdfasdfasdfasdfasdfasd", "dfasdfasdfasdfasdfasdfas" };
-            return text;
-        }
-        if (levelNumber == 5)
-        {
-            string[] text = { "dddd", "sss", "aaa" };
-            return text;
-        }
-        return null;
-    }
-
 }
