972d8d6e5bd2291b2dedef52d35fa22f20fdd514
Gabriel Fuentes <gabrielf9024@gmail.com>
2020-03-18 20:55:58 -0700

    Lots of changes
    Working on dialogue, made title menu with level select, pause menu, pick up with e, all sounds are in, user can use their inventory,borders, moved ui for each map, added end screen

---
+++ Project Haunted Garden/Assets/GetAllObjects.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAllObjects : MonoBehaviour
+{
+    public GameObject[] EnemyObjects;
+    public GameObject[] TowerObjects;
+    public Transform PlayerPosition;
+    public Inventory PlayerInventory;
+    public int LevelIndex;
+    public int BaseHealth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetAllEnemies();
+       // GetPlayerPositionAndInventory();
+       // GetWaveInformation();
+    }
+    public void GetAllEnemies()
+    {
+        EnemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        if (EnemyObjects != null)
+        {
+            GlobalControl.Instance.EnemyObjects = EnemyObjects;
+        }
+    }
+    public void GetPlayerPositionAndInventory()
+    {
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        PlayerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        GlobalControl.Instance.PlayerPosition = PlayerPosition;
+        GlobalControl.Instance.PlayerInventory = PlayerInventory;
+    }
+    public void GetWaveInformation()
+    {
+        LevelIndex = GameObject.FindGameObjectWithTag("LevelSpawner").GetComponent<LevelManager>().levelIndex;
+        GlobalControl.Instance.LevelIndex = LevelIndex;
+    }
+    public void GetAllTowers()
+    {
+        TowerObjects = GameObject.FindGameObjectsWithTag("Tower");
+        GlobalControl.Instance.TowerObjects = TowerObjects;
+    }
+}
---
+++ Project Haunted Garden/Assets/PauseHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Quit() { Application.Quit(); }
+}
